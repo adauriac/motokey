49ffbb4ee41e919631f94ba3d21066f1439b35b3
Jean-Christian Anglès d'Auriac <jeanchristian@anglesdauriac.fr>
2025-12-04 12:14:46 +0100

    grabk.py effectively capture W+F1 ... W+F5

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Binding hotkeys to phrases
 
 Some specific keys send choosen phrases eg email address or paswd
 
-
-On peut utiliser evdev.py pour capter que W+F1 a été capté **même en console sans X11 ni Wayland**
+On peut utiliser evdev.py pour intercepter W+F1 **même en console sans X11 ni Wayland**. Cad qu'il intervient AVANT X11 ou Wayland
+Problème : on ne peut empêcher de re-emettre la touche interceptée que avec dev.grab() qui deonnecte tout le clavier : danger ...
--- grabk.py
+++ grabk.py
@@ -2,36 +2,29 @@ from evdev import InputDevice, UInput, categorize, ecodes
 
 dev = InputDevice('/dev/input/event3')  # Adapter le device
 ui = UInput()                           # Device virtuel
-# dev.grab()                               # Empêche les événements natifs
+dev.grab()                               # Empêche les événements natifs
 
 modifiers = {
     'LEFTMETA': False,
     'RIGHTMETA': False,
 }
 
+KEYCODES = ["KEY_F1","KEY_F2","KEY_F3","KEY_F4","KEY_F5"]
 try:
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY:
             key = categorize(event)
             if key.keystate==2:
                 continue
-            # print(f"{key.keycode=}")
-            # continue
             # Mise à jour de l’état des touches Super
             if key.keycode == 'KEY_LEFTMETA':
                 modifiers['LEFTMETA'] = (key.keystate == key.key_down)
-
             if key.keycode == 'KEY_RIGHTMETA':
                 modifiers['RIGHTMETA'] = (key.keystate == key.key_down)
-
-            if modifiers['LEFTMETA']:
-                print("WITH W")
             # Détection de Super + F1
-            if key.keycode == 'KEY_F1' and key.keystate == key.key_down:
-                print("KEY_F1 enfoncée")
+            if key.keycode in KEYCODES and key.keystate == key.key_down:
                 if modifiers['LEFTMETA'] or modifiers['RIGHTMETA']:
-                    print("SUPER + F1 capturé (bloqué).")
-                    # envoyer votre string ici
+                    print(f"SUPER + {key.keycode} capturé (bloqué).")
                     continue  # NE PAS réémettre ⇒ bloque l’événement natif
                 else:
                     print("sans W ")
